c56362e1d5168fe7560516c90d94eb6efa8acd89
Michaela Křenková <misakren@gmail.com>
2023-04-25 20:28:02 +0200

    Hotový 1. úkol

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -3,6 +3,8 @@ describe('Homework', async () => {
     it('should open page and create screenshot', async () => {
 
         // sem vypracuj domácí úkol
+        await browser.url('/registrace')
+        await browser.saveScreenshot('screenshot.png')
 
     });
 
